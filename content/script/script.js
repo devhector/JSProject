0fdcf21c5751ffac30989b52d29572a8bcfb478e
Hector <hectorhu17@gmail.com>
2020-03-31 11:01:31 -0300

    add listener and build logic

---
+++ content/script/script.js
@@ -0,0 +1,9 @@
+window.addEventListener('keydown', (e) => {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    
+    if (!audio) return; 
+
+    
+
+    audio.play();
+});
