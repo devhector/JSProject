8581905ccebc98bcbbe8ab929964a21ef0aad5df
Hector <hectorhu17@gmail.com>
2020-04-05 16:06:54 -0300

    finish

--- JSProjects/To-Do-List/js/app.js
+++ JSProjects/To-Do-List/js/app.js
@@ -3,6 +3,27 @@ const dateElement = document.getElementById("date");
 const list = document.getElementById("list");
 const input = document.getElementById("input");
 
+let LIST, id;
+
+//dados
+
+const data = localStorage.getItem("TODO");
+
+if(data){
+    LIST = JSON.parse(data);
+    id = LIST.length;
+    loadList(LIST);
+} else{
+    LIST = [];
+    id = 0;
+}
+
+function loadList(array){
+    array.array.forEach(element => {
+        addToDo(element.name, element.id, element.done, element.trash);
+    });
+}
+
 //nome das classes
 
 const CHECK = "fa-check-circle";
@@ -18,13 +39,18 @@ dateElement.innerHTML = today.toLocaleDateString("pt-BR", option);
 
 //funções
 
-function addToDo(ToDo){
+function addToDo(ToDo, id, done, trash){
+
+    if(trash) return;
+
+    const DONE = done ? CHECK : UNCHECK;
+    const LINE = done ? LINETHROUGH : "";
 
     const item = `
     <li class="item">
-    <i class="fa fa-circle-thin co" job="complete" id="0"></i>
-    <p class="text">${ToDo}</p>
-    <i class="fa fa-trash-o de" job="delete" id="0"></i>
+    <i class="fa ${DONE} co" job="complete" id="${id}"></i>
+    <p class="text ${LINE}">${ToDo}</p>
+    <i class="fa fa-trash-o de" job="delete" id="${id}"></i>
     </li>
     `
 
@@ -34,4 +60,63 @@ function addToDo(ToDo){
 }
 
 
-document.addEventListener("keyup", function );
+document.addEventListener("keyup", function (e){
+
+    if(e.keyCode == 13){
+        const ToDo = input.value;
+
+        if(ToDo){
+            addToDo(ToDo, id, false, false);
+
+            LIST.push({
+                name: ToDo,
+                id: id,
+                done: false,
+                trash: false
+
+            });
+
+            localStorage.setItem("TODO", JSON.stringify(LIST));
+
+            id++
+        }
+        
+        input.value = "";
+    }
+
+});
+
+function completeToDo(element){
+    element.classList.toggle(CHECK);
+    element.classList.toggle(UNCHECK);
+    element.parentNode.querySelector(".text").classList.toggle(LINETHROUGH);
+
+    LIST[element.id].done = LIST[element.id].done ? false : true;
+}
+
+function removeToDo(element){
+    element.parentNode.parentNode.removeChild(element.parentNode);
+    LIST[element.id].trash = true;
+}
+
+    list.addEventListener("click", (e) => {
+
+        const element = e.target;
+        const elementJob = element.attributes.job.value;
+
+        if(elementJob == "complete"){
+
+            completeToDo(element);
+
+        }
+        else if(elementJob == "delete"){
+
+            removeToDo(element);
+
+        }
+
+        localStorage.setItem("TODO", JSON.stringify(LIST));
+
+    });
+
+    
